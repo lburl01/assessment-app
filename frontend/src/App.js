32ffda5b2a647eddebc67a36522c9ab0aa15d228
Lori Baumgartner <loribaumgartner@Loris-MacBook-Pro-2.local>
2019-03-22 06:46:22 -0400

    Add ability to update wins/losses and show success and failure toasters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Box, Button, Card, Heading } from '@untappd/components'
+import { Box, Card, Heading, Toaster } from '@untappd/components'
 
-const API = 'http://localhost:5000'
+import WinLossForm from './WinLossForm'
+
+const BASE_URL = 'http://localhost:5000'
 
 class App extends Component {
   constructor(props) {
@@ -10,12 +12,15 @@ class App extends Component {
 
     this.state = {
       conference: null,
+      error: '',
       teams: [],
     }
+
+    this.handleWinsLossesUpdate = this.handleWinsLossesUpdate.bind(this)
   }
 
   fetchData() {
-    axios.get(API, { mode: 'no-cors' }).then(response => {
+    axios.get(BASE_URL, { mode: 'no-cors' }).then(response => {
       const data = response.data
       this.setState({ conference: data.conference, teams: data.teams })
     })
@@ -25,9 +30,30 @@ class App extends Component {
     this.fetchData()
   }
 
+  async handleWinsLossesUpdate(conferenceId, teamId, wins, losses) {
+    this.setState({ error: '' })
+    try {
+      await axios.patch(
+        `${BASE_URL}/conferences/${conferenceId}/teams/${teamId}`,
+        {
+          team: {
+            wins,
+            losses,
+          },
+        },
+        { mode: 'no-cors' },
+      )
+    } catch (error) {
+      this.setState({ error: error.response.data.errors })
+    } finally {
+      return this.state.error
+        ? Toaster.red(this.state.error)
+        : Toaster.green('Success')
+    }
+  }
+
   render() {
-    const { conference } = this.state
-    const { teams } = this.state
+    const { conference, teams } = this.state
 
     if (conference === null) {
       return <h3>loading</h3>
@@ -43,8 +69,14 @@ class App extends Component {
           <Card key={team.id} mb={3}>
             <Card.Header>
               <Heading>
-                {team.name} {team.mascot}
+                {team.name} {team.mascot} • Coached By: {team.coach}
               </Heading>
+            </Card.Header>
+            <Card.Header>
+              <WinLossForm
+                team={team}
+                handleSubmit={this.handleWinsLossesUpdate}
+              />
             </Card.Header>
             <Card.Content>
               <li>Player's</li>
