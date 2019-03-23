7900d4d029979ce9331f0d338943fe624cdcdb3c
Lori Baumgartner <loribaumgartner@Loris-MacBook-Pro-2.local>
2019-03-23 08:30:10 -0400

    Add ability to update player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,27 @@
 class PlayersController < ApplicationController
+  before_action :set_team
   # GET /players
   def index
-    team = Team.find_by(id: params[:team_id], conference_id: params[:conference_id])
-    @players = team.players
+    @players = @team.players
 
     render json: @players
+  end
+
+  def update
+    @player = Player.find(params[:id])
+
+    if @player.update_attributes(player_params)
+      render json: @player
+    else
+      render json: { errors: @player.errors.full_messages }
+    end
+  end
+
+  private def player_params
+    params.require(:player).permit(:jersey_number)
+  end
+
+  private def set_team
+    @team = Team.find_by(id: params[:team_id], conference_id: params[:conference_id])
   end
 end
