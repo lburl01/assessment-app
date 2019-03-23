cb47c0fe210d9f4ee7d66970de658e37637d7d35
Lori Baumgartner <loribaumgartner@Loris-MacBook-Pro-2.local>
2019-03-23 09:14:58 -0400

    WIP: refactor player list into child component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import {
-  Box,
-  Card,
-  Heading,
-  Icons,
-  Link,
-  List,
-  ListItem,
-  TextInput,
-  Toaster,
-} from '@untappd/components'
+import { Box, Card, Heading, Link, Toaster } from '@untappd/components'
 
 import WinLossForm from './WinLossForm'
+import TeamPlayerList from './TeamPlayerList'
 
-const BASE_URL = 'http://localhost:5000'
-const NUM_INPUT_VALIDATION_ERROR = 'Values must be positive, whole numbers.'
+export const BASE_URL = 'http://localhost:5000'
+export const NUM_INPUT_VALIDATION_ERROR =
+  'Values must be positive, whole numbers.'
 
 class App extends Component {
   constructor(props) {
@@ -24,13 +16,11 @@ class App extends Component {
     this.state = {
       conference: null,
       error: '',
-      jerseyErrors: {},
       isFetching: false,
       isSubmitting: false,
       teams: [],
     }
 
-    this.clearJerseyError = this.clearJerseyError.bind(this)
     this.handleWinsLossesUpdate = this.handleWinsLossesUpdate.bind(this)
     this.toggleShouldShowPlayers = this.toggleShouldShowPlayers.bind(this)
   }
@@ -118,44 +108,6 @@ class App extends Component {
     }
   }
 
-  clearJerseyError(playerId) {
-    return this.setState({
-      jerseyErrors: { ...this.state.jerseyErrors, [playerId]: '' },
-    })
-  }
-
-  async handleJerseyUpdate(conferenceId, player, newVal) {
-    this.clearJerseyError(player.id)
-    if (!newVal || newVal < 0 || !Number.isInteger(Number(newVal))) {
-      return this.setState({
-        jerseyErrors: {
-          ...this.state.jerseyErrors,
-          [player.id]: NUM_INPUT_VALIDATION_ERROR,
-        },
-      })
-    }
-    const teamId = player.team_id
-    try {
-      await axios.patch(
-        `${BASE_URL}/conferences/${conferenceId}/teams/${teamId}/players/${
-          player.id
-        }`,
-        {
-          player: {
-            jersey_number: newVal,
-          },
-        },
-        { mode: 'no-cors' },
-      )
-    } catch (error) {
-      this.setState({ error: error.response.data.errors })
-    } finally {
-      return this.state.error
-        ? Toaster.red(this.state.error)
-        : this.clearJerseyError(player.id)
-    }
-  }
-
   toggleShouldShowPlayers(conferenceId, teamId) {
     const stateCopy = JSON.parse(JSON.stringify(this.state))
     const newTeamsState = stateCopy.teams.map(team => {
@@ -204,36 +156,10 @@ class App extends Component {
               />
             </Card.Header>
             {team.shouldShowPlayers && !isFetching && (
-              <List>
-                {team.players.map(player => (
-                  <ListItem key={player.id}>
-                    <ListItem.Content>
-                      <ListItem.Heading>{player.name}</ListItem.Heading>
-                      <ListItem.Info>
-                        Jersey:
-                        <TextInput
-                          defaultValue={player.jersey_number}
-                          error={this.state.jerseyErrors[player.id]}
-                          id={`jersey-input-${player.id}`}
-                          min={0}
-                          name="jersey-number"
-                          onChange={e =>
-                            this.handleJerseyUpdate(
-                              team.conference_id,
-                              player,
-                              e.target.value,
-                            )
-                          }
-                          type="number"
-                        />
-                      </ListItem.Info>
-                      <ListItem.Info>Position: {player.position}</ListItem.Info>
-                      <ListItem.Info>Height: {player.height}</ListItem.Info>
-                      <ListItem.Info>Weight: {player.weight}</ListItem.Info>
-                    </ListItem.Content>
-                  </ListItem>
-                ))}
-              </List>
+              <TeamPlayerList
+                handleUpdate={this.handleJerseyUpdate}
+                team={team}
+              />
             )}
           </Card>
         ))}
---
+++ frontend/src/TeamPlayerList.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import { List, ListItem, TextInput, Toaster } from '@untappd/components'
+
+import { BASE_URL, NUM_INPUT_VALIDATION_ERROR } from './App'
+
+class TeamPlayerList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      responseError: '',
+      errors: {},
+    }
+
+    this.clearErrors = this.clearErrors.bind(this)
+    this.handleUpdate = this.handleUpdate.bind(this)
+  }
+
+  clearErrors(playerId) {
+    return this.setState({
+      errors: { ...this.state.errors, [playerId]: '' },
+    })
+  }
+
+  async handleUpdate(conferenceId, player, newVal) {
+    this.clearErrors(player.id)
+    if (!newVal || newVal < 0 || !Number.isInteger(Number(newVal))) {
+      return this.setState({
+        errors: {
+          ...this.state.errors,
+          [player.id]: NUM_INPUT_VALIDATION_ERROR,
+        },
+      })
+    }
+    const teamId = player.team_id
+    try {
+      await axios.patch(
+        `${BASE_URL}/conferences/${conferenceId}/teams/${teamId}/players/${
+          player.id
+        }`,
+        {
+          player: {
+            jersey_number: newVal,
+          },
+        },
+        { mode: 'no-cors' },
+      )
+    } catch (error) {
+      this.setState({ responseError: error.response.data.errors })
+    } finally {
+      return this.state.responseError
+        ? Toaster.red(this.state.responseError)
+        : this.clearErrors(player.id)
+    }
+  }
+
+  render() {
+    const { team } = this.props
+    return (
+      <List>
+        {team.players.map(player => (
+          <ListItem key={player.id}>
+            <ListItem.Content>
+              <ListItem.Heading>{player.name}</ListItem.Heading>
+              <ListItem.Info>
+                Jersey:
+                <TextInput
+                  defaultValue={player.jersey_number}
+                  error={this.state.errors[player.id]}
+                  id={`jersey-input-${player.id}`}
+                  min={0}
+                  name="jersey-number"
+                  onChange={e =>
+                    this.handleUpdate(
+                      team.conference_id,
+                      player,
+                      e.target.value,
+                    )
+                  }
+                  type="number"
+                />
+              </ListItem.Info>
+              <ListItem.Info>Position: {player.position}</ListItem.Info>
+              <ListItem.Info>Height: {player.height}</ListItem.Info>
+              <ListItem.Info>Weight: {player.weight}</ListItem.Info>
+            </ListItem.Content>
+          </ListItem>
+        ))}
+      </List>
+    )
+  }
+}
+
+export default TeamPlayerList
