0d1ac0a081f2ee16d37546a0cfb43f9b388c44a1
Lori Baumgartner <loribaumgartner@Loris-MacBook-Pro-2.local>
2019-03-24 16:26:50 -0400

    Add validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,6 @@ class Player < ApplicationRecord
   belongs_to :team
 
   validates_uniqueness_of :jersey_number, scope: :team_id
+  validates :jersey_number, numericality: { only_integer: true }
+  validates :weight, numericality: true
 end
