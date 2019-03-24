9750740376ac3436c540c0bfd9f4083782a988cd
Lori Baumgartner <loribaumgartner@Loris-MacBook-Pro-2.local>
2019-03-24 19:37:06 -0400

    Make a height formatter; clean up alignment

--- frontend/src/PlayerListItem.jsx
+++ frontend/src/PlayerListItem.jsx
@@ -1,27 +1,38 @@
 import React from 'react'
 
-import { ListItem, TextInput, Toggle } from '@untappd/components'
+import { Box, FormLabel, ListItem, TextInput, Toggle } from '@untappd/components'
+
+const heightFormatter = height => {
+  const heightVals = height.split('-')
+  return `${heightVals[0]}' ${heightVals[1]}"`
+}
 
 const PlayerListItem = ({ errors, handlePlayerUpdate, player }) => (
   <ListItem>
     <ListItem.Content>
-      <ListItem.Heading>{player.name}</ListItem.Heading>
-      <ListItem.Info>
-        Jersey:
+      <ListItem.Heading fontSize={3}>{player.name}</ListItem.Heading>
+      <ListItem.Info>{player.position}</ListItem.Info>
+      <ListItem.Info>{heightFormatter(player.height)}</ListItem.Info>
+      <ListItem.Info>{player.weight} lbs.</ListItem.Info>
+    </ListItem.Content>
+    <ListItem.Actions>
+      <Box mr={3}>
+        <FormLabel>Jersey Number</FormLabel>
         <TextInput
           defaultValue={player.jersey_number}
           error={errors[player.id]}
           id={`jersey-input-${player.id}`}
           min={0}
+          width={[1, 1 / 2]}
           name="jersey-number"
           onBlur={e =>
             handlePlayerUpdate('jersey_number', player, e.target.value)
           }
           type="number"
         />
-      </ListItem.Info>
-      <ListItem.Info>
-        Starter:
+      </Box>
+      <Box>
+        <FormLabel>Starter</FormLabel>
         <Toggle
           height={24}
           id={`starter=${player.id}`}
@@ -29,11 +40,8 @@ const PlayerListItem = ({ errors, handlePlayerUpdate, player }) => (
           checked={player.starter}
           onChange={val => handlePlayerUpdate('starter', player, val)}
         />
-      </ListItem.Info>
-      <ListItem.Info>Position: {player.position}</ListItem.Info>
-      <ListItem.Info>Height: {player.height}</ListItem.Info>
-      <ListItem.Info>Weight: {player.weight}</ListItem.Info>
-    </ListItem.Content>
+      </Box>
+    </ListItem.Actions>
   </ListItem>
 )
 
