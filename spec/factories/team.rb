46d2529d056f1000a87e1c0b1336e2d68bc016cb
Lori Baumgartner <loribaumgartner@Loris-MacBook-Pro-2.local>
2019-03-24 15:57:56 -0400

    Add team factory

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -1,7 +1,11 @@
-# frozen_string_literal: true
-
 FactoryBot.define do
   factory :team do
+    name { 'UNC' }
+    mascot { 'Ram' }
+    coach { 'Roy Williams' }
+    wins { 1 }
+    losses { 1 }
+
     trait :with_players do
     end
   end
